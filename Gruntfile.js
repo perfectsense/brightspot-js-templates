60f71d2328f92e0276f11565a5c5684789a79c6b
Rusty Tanton <rusty@perfectsensedigital.com>
2015-06-12 09:45:54 -0400

    added Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+	
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['compile']);
+	grunt.registerTask('test', ['jshint:all']);
+	
+	grunt.registerTask('compile', ['compilejs']);
+	grunt.registerTask('compilejs', ['requirejs:dist','uglify:dist']);
+
+	grunt.initConfig({
+		requirejs: {
+			dist: {
+				options: {
+					baseUrl: 'src/js',
+					include: ['bsp-templates'],
+					paths: {
+						'jquery': 'empty:'
+					},
+					optimize: 'none',
+					out: 'dist/bsp-templates.js',
+					wrap: true
+				}
+			}
+		},
+		uglify: {
+			dist: {
+				files: {
+					'dist/bsp-templates.min.js' : ['dist/bsp-templates.js'],
+				}
+			}
+		},
+		jshint: {
+			all: ['Gruntfile.js', 'src/js/**/*.js']
+  		},
+		watch: {
+			src: {
+				files: ['src/**/*'],
+				tasks: ['default']
+			}
+		}
+	});
+
+};

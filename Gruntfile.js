bc7f6ca79bbf1350d5387b403be43e31cec7e51a
Rusty Tanton <rusty@perfectsensedigital.com>
2015-06-26 10:38:21 -0400

    converted to es6

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,22 @@
 module.exports = function(grunt) {
 	
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('default', ['compile']);
+	grunt.registerTask('default', ['copy:dist']);
 	grunt.registerTask('test', ['jshint:all']);
-	
-	grunt.registerTask('compile', ['compilejs']);
-	grunt.registerTask('compilejs', ['copy:dist','uglify:dist']);
 
 	grunt.initConfig({
 		copy: {
 			dist: {
-				src: ['src/js/bsp-template-plugin.js'],
+				src: [
+					'bower_components/handlebars/handlebars.js',
+					'src/js/bsp-template-plugin.js'
+				],
 				dest: 'dist/',
 				expand: true,
 				flatten: true
-			}
-		},
-		uglify: {
-			dist: {
-				files: {
-					'dist/bsp-template-plugin.min.js' : ['dist/bsp-template-plugin.js']
-				}
 			}
 		},
 		jshint: {
--- src/js/bsp-template-plugin.js
+++ src/js/bsp-template-plugin.js
@@ -1,59 +1,46 @@
 /*
  * Plugin to load bsp-templates
- *
- * bsp-templates is generated with 
- * https://github.com/perfectsense/brightspot-js-grunt/
  */
-(function(globals, factory) {
+import $ from 'jquery';
+import bsp_utils from 'bsp_utils';
+import Handlebars from './handlebars';
 
-    "use strict";
-
-    define(['jquery','bsp-utils','handlebars'], factory);
-
-})(this, function($, bsp_utils, Handlebars, globals) {
-
-    "use strict";
-
-    var module = {
-        init: function($el, options) {
-            var dataLoaded;
-            var templateLoaded;
-            if (typeof options.template !== 'string') {
-                return;
+export default bsp_utils.plugin(globals, 'bsp', 'template', {
+    '_each': function(item) {
+        var options = this.option(item);
+        var moduleInstance = Object.create({
+            init: function($el, options) {
+                var dataLoaded;
+                var templateLoaded;
+                if (typeof options.template !== 'string') {
+                    return;
+                }
+                templateLoaded = $.get('/render/' + options.template + '.hbs');
+                if (typeof options.data === 'object') {
+                    $.when(templateLoaded).then(function(template) {
+                        if ($.isArray(template)) {
+                            template = template[0];
+                        }
+                        $el.html( Handlebars.compile(template)(options.data) );
+                    });
+                } else if (typeof options.dataUrl === 'string') {
+                    dataLoaded = $.get(options.dataUrl);
+                    $.when(templateLoaded, dataLoaded).then(function(template, data) {
+                        if ($.isArray(template)) {
+                            template = template[0];
+                        }
+                        if ($.isArray(data)) {
+                            data = data[0];
+                        }
+                        $el.html( Handlebars.compile(template)(data) );
+                    });
+                } else {
+                    $.when(templateLoaded).then(function(template) {
+                        $el.html( Handlebars.compile(template)({}) ); 
+                    });
+                }
             }
-            templateLoaded = $.get('/render/' + options.template + '.hbs');
-            if (typeof options.data === 'object') {
-                $.when(templateLoaded).then(function(template) {
-                    if ($.isArray(template)) {
-                        template = template[0];
-                    }
-                    $el.html( Handlebars.compile(template)(options.data) );
-                });
-            } else if (typeof options.dataUrl === 'string') {
-                dataLoaded = $.get(options.dataUrl);
-                $.when(templateLoaded, dataLoaded).then(function(template, data) {
-                    if ($.isArray(template)) {
-                        template = template[0];
-                    }
-                    if ($.isArray(data)) {
-                        data = data[0];
-                    }
-                    $el.html( Handlebars.compile(template)(data) );
-                });
-            } else {
-                $.when(templateLoaded).then(function(template) {
-                    $el.html( Handlebars.compile(template)({}) ); 
-                });
-            }
-        }
-    };
-
-    return bsp_utils.plugin(globals, 'bsp', 'template', {
-        '_each': function(item) {
-            var options = this.option(item);
-            var moduleInstance = Object.create(module);
-            moduleInstance.init($(item), options);
-        }
-    });
-
+        });
+        moduleInstance.init($(item), options);
+    }
 });

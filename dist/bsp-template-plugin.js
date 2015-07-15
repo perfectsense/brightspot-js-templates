117156ce62ff327bdd79e9911c1d1463bae65929
Rusty Tanton <rusty@perfectsensedigital.com>
2015-07-15 08:04:22 -0400

    fixed typo

--- dist/bsp-template-plugin.js
+++ dist/bsp-template-plugin.js
@@ -8,7 +8,7 @@ import bsp_template from 'bsp-template';
 export default bsp_utils.plugin(false, 'bsp', 'template', {
     '_each': function(item) {
         var options = this.option(item);
-        var moduleInstance = Object.create(thePlugin);
+        var moduleInstance = Object.create(bsp_template);
         moduleInstance.init($(item), options).fetch().then(() => {
             moduleInstance.render();
         });
